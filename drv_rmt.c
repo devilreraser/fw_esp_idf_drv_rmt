f8def4393288f6f510c0bbf3f014d3cb0147b091
Dimitar Lilov <devilerser@gmail.com>
2024-04-05 01:13:01 +0300

    Try add idf ver 5.2.1

--- drv_rmt.c
+++ drv_rmt.c
@@ -114,7 +114,7 @@ size_t drv_rmt_test_read_rx(uint8_t* pu8_data, size_t max_size, TickType_t timeo
 
     items = (rmt_item32_t *) xRingbufferReceive(test_rb, &length, timeout);
 
-    if (timeout) vTaskDelay(10);
+    if (timeout) vTaskDelay(100);
     if (items) 
     {
         if (timeout)
